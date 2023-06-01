022e26763457fcb9b9dc83753b539604c86503a7
bccarruth927 <brecarr1997@aol.com>
2023-06-01 08:18:37 -0400

    Add Screen content and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 //Import the Wrapper Component
 import Wrapper from './components/Wrapper';
+//Import the Screen Component
+import Screen from './components/Screen';
 
 function App() {
   return (
     <div>
       <Wrapper>
+        <Screen />
         <h1>App</h1>
       </Wrapper>
     </div>
---
+++ src/components/Screen.js
@@ -0,0 +1,11 @@
+//Import CSS styling from Screen.module.css
+import styles from './Screen.module.css';
+
+//Main Screen Component
+function Screen() {
+    return (
+        <div className={styles.screen}>0123456</div>
+    );
+};
+
+export default Screen;
